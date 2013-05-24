252efb7a35c14b05825a25db1815c8dacb7d7fc7
Lucian Postelnicu <lucian.coding@gmail.com>
2013-05-24 17:56:48 +0300

    adding initial files

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+	// Setup the main slider on homepage
+	/*var sirulMeu = new Array('o fabrica', 'timpuri noi', 'imgb', 'inna scheffler');
+	console.log("sirul este: ", sirulMeu);
+	console.log("sirul are: ", sirulMeu.length, " elemenete");
+	console.log("eu locuiesc la... ", sirulMeu[1]);
+
+	var all = sirulMeu.length;
+	var i;
+  	for(i=0; i<all; i=i+1){
+    	console.log("la adresa " + sirulMeu[i] + " locuiesc ", i+20+Math.random());
+  	}*/
+	
+});
